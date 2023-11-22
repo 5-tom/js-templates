de118c62f5ae9d99f1d3ef2e6d722cb37ed4028e
tomhonour <tom.honour@illuminetsolutions.com>
2023-11-22 11:28:20 +0000

    typescript

--- server.ts
+++ server.ts
@@ -2,14 +2,17 @@ import express, { Router } from "express";
 const api = express();
 const app = Router();
 //
+
 app.get("/hello", (req, res) => {
-    res.send("hello");
+	res.send("hello");
 });
+
 app.get("/world", (req, res) => {
-    res.send("world");
+	res.send("world");
 });
+
 //
 api.use("/api", app);
 api.listen(3000, () => {
-    console.log("running on 3000");
+	console.log("running on 3000");
 });
